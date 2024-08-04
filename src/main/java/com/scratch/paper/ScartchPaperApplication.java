c746be2afe3b992212518ca7af8c2a6f6ac54925
sslee <sslee@kr.doubledown.com>
2024-08-04 20:46:21 +0900

    first commit

---
+++ src/main/java/com/scratch/paper/ScartchPaperApplication.java
@@ -0,0 +1,13 @@
+package com.scratch.paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScartchPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScartchPaperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scratch/paper/ScartchPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scratch.paper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScartchPaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
